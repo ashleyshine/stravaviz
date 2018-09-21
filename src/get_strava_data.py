85022ff875f4605267cfabe8dace5bdb687af50b
ashleyshine <ashleyschang@gmail.com>
2018-09-20 22:33:36 -0700

    Module to interface with Strava's API

---
+++ src/get_strava_data.py
@@ -0,0 +1,38 @@
+import logging
+
+import polyline
+import stravalib
+
+import src.strava_config as strava_config
+
+# Raise logger level since Strava frequently changes the structure of
+# their models, resulting in (relatively harmless) warnings.
+logger = logging.getLogger()
+logger.setLevel('ERROR')
+
+
+def create_client():
+    """Creates client to interface with Strava API."""
+    return stravalib.client.Client(strava_config.ACCESS_TOKEN)
+
+
+def get_activity_polylines(before=None, after=None, limit=None):
+    """Gets activity polylines for user.
+
+    Args:
+        before (datetime or str): Retrieve activities before this date.
+            Format YYYY-MM-DD.
+        after (datetime or str): Retrieve activities after this date.
+            Format YYYY-MM-DD.
+        limit (int): Maximum number of activities to return.
+
+    Returns:
+        A list of polyline strings
+    """
+    client = create_client()
+    activities = client.get_activities(before=before, after=after, limit=limit)
+    decoded_polylines = [
+        polyline.decode(activity.map.summary_polyline)
+        for activity in activities
+    ]
+    return decoded_polylines

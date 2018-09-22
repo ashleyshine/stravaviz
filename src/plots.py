affe79019a4fe93928183cd204bc5bd01f660a53
ashleyshine <ashleyschang@gmail.com>
2018-09-21 20:44:40 -0700

    Add function to create facet plots

--- src/plots.py
+++ src/plots.py
@@ -7,22 +7,27 @@ from collections import namedtuple
 from src.get_strava_data import get_activity_polylines
 
 
-def create_heatmap(polylines, style='black', color='deepskyblue', save=False,
-                   img_filename='heatmap.png'):
+def create_heatmap(activity_params={}, style='black', color='deepskyblue',
+                   save=False, img_filename='heatmap.png'):
     """Creates static heatmap with activity polylines.
 
     Args:
-        polylines (list): List of polylines to plot on map.
-        style (str): Optional, background color of map. Either 'light'
-            or 'dark'.
+        activity_params (dict): Optional parameters for retrieved
+            activities. Format below. Default plots all activities.
+            {
+                'after': 'YYYY-MM-DD',
+                'before': 'YYYY-MM-DD',
+                'limit': int
+            }
+        style (str): Optional, background color of map.
         color (str): Optional, color of activity polylines.
         save (boolean): Optional parameter, whether to save plot.
-        img_filename (str): Optional, name of image, if image is to be
-            saved.
+        img_filename (str): Optional, name of image.
 
     Returns:
         None
     """
+    polylines = get_activity_polylines(**activity_params)
     latitude, longitude = extract_lat_long(polylines)
 
     fig, ax = plt.subplots(figsize=(24, 18))
@@ -53,6 +58,56 @@ def extract_lat_long(polylines):
         longitude += lon
 
     return Lat_Long(latitude, longitude)
+
+
+def create_facet_plot(activity_params={}, style='white', color='black',
+                      save=False, img_filename='facet_plot.png'):
+    """Creates facet plot with individual activities.
+
+    Args:
+        activity_params (dict): Optional parameters for retrieved
+            activities. Format below. Default plots all activities.
+            {
+                'after': 'YYYY-MM-DD',
+                'before': 'YYYY-MM-DD',
+                'limit': int
+            }
+        style (str): Optional, background color of map.
+        color (str): Optional, color of activity polylines.
+        save(boolean): Optional parameter, whether to save plot
+        img_filename (str): Optional, name of image.
+
+    Returns:
+        None
+    """
+    polylines = get_activity_polylines(**activity_params)
+    sq_dim = facet_plot_dimensions(len(polylines))
+
+    fig, axes = plt.subplots(figsize=(24, 24), nrows=sq_dim, ncols=sq_dim)
+    fig.set_facecolor(style)
+
+    polyline_iterator = iter(polylines)
+    for row in axes:
+        for col in row:
+            try:
+                lat, lon = zip(*polyline_iterator.__next__())
+                col.plot(lat, lon, color='black', lw=0.7)
+            except StopIteration:
+                pass
+            col.axis('off')
+            plt.axis('equal')
+
+
+def facet_plot_dimensions(n_activities):
+    """Returns dimension N of a N x N facet plot that fits n_activities.
+
+    Args:
+        n_activities (int): Number of activities to be plotted.
+
+    Returns:
+        N (int), where N is the number of rows and columns to be used.
+    """
+    return min(i for i in range(100) if i**2 >= n_activities)
 
 
 def save_figure(fig, filename):
